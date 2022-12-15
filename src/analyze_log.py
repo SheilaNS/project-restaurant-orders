193b6e5104ff8d08964bbd5f5158863d1ea83886
SheilaNS <shei.nsantos@gmail.com>
2022-12-15 17:57:07 -0300

    feat(requisito 1.1): "Quantas vezes 'arnaldo' pediu 'hamburguer'?" done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -7,6 +7,15 @@ def get_most_ordered(orders, customer):
     return Counter(customer_orders).most_common()[0][0]
 
 
+def get_customer_count_dish(orders, customer, dish):
+    custumer_orders = [
+        order[1]
+        for order in orders
+        if order[0] == customer and order[1] == dish
+    ]
+    return len(custumer_orders)
+
+
 def analyze_log(path_to_file):
     if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
@@ -18,6 +27,9 @@ def analyze_log(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
     maria_most_ordered_dish = get_most_ordered(orders, "maria")
+    count_arnaldo_burgers = get_customer_count_dish(
+        orders, "arnaldo", "hamburguer"
+    )
 
     with open("data/mkt_campaign.txt", mode="w", encoding="utf-8") as file:
-        file.write(f"{maria_most_ordered_dish}\n")
+        file.write(f"{maria_most_ordered_dish}\n" f"{count_arnaldo_burgers}\n")
