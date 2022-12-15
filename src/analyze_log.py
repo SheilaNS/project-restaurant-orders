d351de457106dcce9367b00f809acfcd44c2d37d
SheilaNS <shei.nsantos@gmail.com>
2022-12-15 17:44:05 -0300

    feat(requisito 1.1): "Qual o prato mais pedido por 'maria'?" done

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,10 @@
 import csv
+from collections import Counter
+
+
+def get_most_ordered(orders, customer):
+    customer_orders = [order[1] for order in orders if order[0] == customer]
+    return Counter(customer_orders).most_common()[0][0]
 
 
 def analyze_log(path_to_file):
@@ -8,6 +14,10 @@ def analyze_log(path_to_file):
     try:
         with open(path_to_file, mode="r", encoding="utf-8") as file:
             orders = [*csv.reader(file)]
-            return orders
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
+
+    maria_most_ordered_dish = get_most_ordered(orders, "maria")
+
+    with open("data/mkt_campaign.txt", mode="w", encoding="utf-8") as file:
+        file.write(f"{maria_most_ordered_dish}\n")
