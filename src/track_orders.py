d532be16525f415f88eec124fb0ee8578e01a5da
SheilaNS <shei.nsantos@gmail.com>
2022-12-15 18:51:57 -0300

    feat(requisito 2): get_never_ordered_per_customer done

--- src/track_orders.py
+++ src/track_orders.py
@@ -19,7 +19,11 @@ class TrackOrders:
         return Counter(customer_orders).most_common()[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        dishes = set(order[1] for order in self.orders)
+        customer_orders = set(
+            order[1] for order in self.orders if order[0] == customer
+        )
+        return dishes - customer_orders
 
     def get_days_never_visited_per_customer(self, customer):
         pass
