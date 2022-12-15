03ab98a7c0ab6a24423bd084b75414d8d4313c65
SheilaNS <shei.nsantos@gmail.com>
2022-12-15 15:48:23 -0300

    feat(requisito 1.2): inexisting file implemented

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,3 +1,13 @@
+import csv
+
+
 def analyze_log(path_to_file):
-    if not path_to_file.endswith('.csv'):
+    if not path_to_file.endswith(".csv"):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
+
+    try:
+        with open(path_to_file, mode="r", encoding="utf-8") as file:
+            orders = [*csv.reader(file)]
+            return orders
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
